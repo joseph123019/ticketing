388d8d13c08b8a945a244169c491d4bc4acf71fd
Joseph Munoz <josephmunoz191230@gmail.com>
2023-09-23 11:57:11 -0400

    update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 
 export default ({ req }) => {
-  if (typeof window === 'undefined') {
+  // Determine if the code is running on the server
+  const isServer = typeof window === 'undefined';
+
+  if (isServer) {
     // We are on the server
-
-    // Disable SSL certificate verification (not recommended for production)
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-    return axios.create({
+    const axiosInstance = axios.create({
       baseURL: 'https://www.ticketing.sepingel.com',
       headers: req.headers,
     });
+
+    // Configure axios instance to reject invalid SSL certificates
+    axiosInstance.defaults.httpsAgent = new https.Agent({
+      rejectUnauthorized: true, // Enable SSL certificate verification
+    });
+
+    return axiosInstance;
   } else {
     // We must be on the browser
     return axios.create({
